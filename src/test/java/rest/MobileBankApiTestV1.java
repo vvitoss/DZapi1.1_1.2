603fea3d34dba25b923833ba9752d101e4e9b9fb
vvitoss <orangekaramel@gmail.com>
2022-03-25 08:32:48 +0300

    initial commit

---
+++ src/test/java/rest/MobileBankApiTestV1.java
@@ -0,0 +1,26 @@
+package rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+}
