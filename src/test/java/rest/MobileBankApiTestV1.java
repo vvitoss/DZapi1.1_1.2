ce3db0c42c87867df70c2fd742a577f8114bf314
vvitoss <orangekaramel@gmail.com>
2022-03-25 09:58:20 +0300

    upgreid test

--- src/test/java/rest/MobileBankApiTestV1.java
+++ src/test/java/rest/MobileBankApiTestV1.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-class MobileBankApiTestV {
+class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
